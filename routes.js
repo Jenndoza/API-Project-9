145f0af2e159c00095715694d020fe3fcaf45513
Jenndoza <j.mendo1997@gmail.com>
2022-08-22 03:37:59 -0500

    added User Routes to routes.js

--- routes.js
+++ routes.js
@@ -1,24 +1,63 @@
-'use strict';
+"use strict";
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const router = express.Router();
-
 //const {model} = require("./models")
-const {User,Course} = require("./models")
+const { User, Course } = require("./models");
 
-router.use(express.json())
+router.use(express.json());
 
-//Get router
-router.get('/', (req, res) => {
-    res.json({
-      message: 'Welcome to the REST API project!',
-    });
-  });
+//Handler function for all routes
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
+
+//User Get Route that returns a list of users
+router.get(
+  "/users",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    const user = await req.currentUser;
+    res.json(user).status(200);
+  })
+);
+
+//User Post Route that creates a new user
+router.post(
+  "/users",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body);
+      res.status(201).location("/").end();
+    } catch (error) {
+      console.log("ERROR: ", error.name);
+
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+//Courses GET route that will return all courses inluding User associated w that course
 
 
 
 
-module.exports= router
+module.exports = router;
